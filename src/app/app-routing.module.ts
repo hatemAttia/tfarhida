684b0f034476699ab7c3051ff114eb8015028858
hatemAttia <attiahatem@outlook.com>
2021-07-03 23:29:12 +0100

    update acceuil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -8,9 +9,10 @@ const routes: Routes = [
     },
   ];
   
-@NgModule({
-  imports: [],
-  exports: [RouterModule]
-})
+  @NgModule({
+    declarations: [],
+    imports: [CommonModule, RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
 
 export class AppRoutingModule { }
