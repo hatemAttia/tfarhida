f1401c59a15e773d5a2793de3f4638f1ed8b6b79
hatemAttia <attiahatem@outlook.com>
2021-07-04 14:38:14 +0100

    update first interface

---
+++ src/app/modules/best-offers/best-offers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-best-offers',
+  templateUrl: './best-offers.component.html',
+  styleUrls: ['./best-offers.component.css']
+})
+export class BestOffersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
